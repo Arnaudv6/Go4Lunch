277f518bf481b806ae6a4ff18769de9c7b26fcd1
Arnaudv6 <Arnaudv6@users.noreply.github.com>
2021-09-22 13:43:51 +0200

    newInstance() working in kotlin, centerOnMe working

--- app/src/main/java/com/cleanup/go4lunch/MainActivity.kt
+++ app/src/main/java/com/cleanup/go4lunch/MainActivity.kt
@@ -24,18 +24,19 @@ class MainActivity : AppCompatActivity() {
         requestPermissionsIfNecessary(
             arrayOf(
                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE,
                 Manifest.permission.INTERNET,
                 Manifest.permission.ACCESS_FINE_LOCATION,
-                Manifest.permission.ACCESS_NETWORK_STATE
+                Manifest.permission.ACCESS_NETWORK_STATE,
+                Manifest.permission.ACCESS_COARSE_LOCATION
             )
         )
 
         setContentView(R.layout.activity_main)
 
         if (savedInstanceState == null) {
-            //todo replace with newInstance()
             supportFragmentManager.beginTransaction()
-                .add(R.id.fragment_container, MapFragment(), null).commit()
+                .add(R.id.fragment_container, MapFragment.newInstance(this), null).commit()
         }
 
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
--- app/src/main/java/com/cleanup/go4lunch/ui/map/MapFragment.kt
+++ app/src/main/java/com/cleanup/go4lunch/ui/map/MapFragment.kt
@@ -14,8 +14,6 @@ import androidx.lifecycle.lifecycleScope
 import com.cleanup.go4lunch.BuildConfig
 import com.cleanup.go4lunch.R
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.flow
 import org.osmdroid.bonuspack.location.NominatimPOIProvider
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -33,11 +31,13 @@ class MapFragment() : Fragment() {
     private lateinit var map: MapView
     private lateinit var locationOverlay: MyLocationNewOverlay  // SimpleLocationOverlay is noop
 
-    fun newInstance(context: Context): MapFragment {
-        // val args = Bundle()
-        val fragment = MapFragment()
-        // fragment.arguments = args
-        return fragment
+    companion object{
+        fun newInstance(context: Context): MapFragment {
+            // val args = Bundle()
+            val fragment = MapFragment()
+            // fragment.arguments = args
+            return fragment
+        }
     }
 
     override fun onCreateView(
@@ -47,7 +47,7 @@ class MapFragment() : Fragment() {
     ): View {
         mMapViewModel = ViewModelProvider(this).get(MapViewModel::class.java)
 
-        val view: View = inflater.inflate(R.layout.fragment_home, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_map, container, false)
 
         Configuration.getInstance().load(
             context,
@@ -58,12 +58,10 @@ class MapFragment() : Fragment() {
         map = view.findViewById(R.id.map)
         map.setTileSource(TileSourceFactory.MAPNIK)
         map.setMultiTouchControls(true)
-        val mapController = map.controller
         map.zoomController.setVisibility(CustomZoomButtonsController.Visibility.NEVER)
 
         map.isTilesScaledToDpi = true
         map.isVerticalMapRepetitionEnabled = false
-
 
         map.setScrollableAreaLimitLatitude(
             TileSystem.MaxLatitude,
@@ -71,14 +69,16 @@ class MapFragment() : Fragment() {
             0/*map.getHeight()/2*/
         )
 
+        map.controller.setZoom(4.0)
+        // var loc = GeoPoint(48.8583, 2.2944)
+
 //        GpsMyLocationProvider(context)
 
-
-        // if given a position, will be in direction mode
         val icon =
             ResourcesCompat.getDrawable(resources, R.drawable.ic_baseline_my_location_24, null)
                 ?.toBitmap(64, 64)
 
+        // if given a position, will be in direction mode
         locationOverlay = MyLocationNewOverlay(map)
         // todo consider setting location providers
         locationOverlay.disableFollowLocation()
@@ -108,8 +108,6 @@ class MapFragment() : Fragment() {
         }
 */
 
-        // todo  il faut un bouton de centrage sur ma position
-
         // add scale bar
         val mScaleBarOverlay = ScaleBarOverlay(map)
         mScaleBarOverlay.setAlignRight(true)
@@ -120,36 +118,21 @@ class MapFragment() : Fragment() {
         return view
     }
 
-    suspend fun getLocationUpdates (): GeoPoint{
-        delay(1000)
-        return locationOverlay.myLocation
-    }
-
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val centerOnMe = view.findViewById<AppCompatImageButton>(R.id.center_on_me)
-
-        var loc = GeoPoint(48.8583, 2.2944);
-
-        viewLifecycleOwner.lifecycleScope.launchWhenStarted() {
-            flow {
-                emit(getLocationUpdates())
-            }.collect {
-                while (true) {
-                    loc = it
-                }
+        centerOnMe.setOnClickListener {
+            viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+                suspend {
+                    do {
+                        val loc: GeoPoint? = locationOverlay.myLocation
+                        map.controller.animateTo(loc, 15.0, 1)
+                        delay(100)
+                    } while (loc == null)
+                }.invoke()
             }
         }
-
-        centerOnMe.setOnClickListener(View.OnClickListener { v -> centerOnMe(loc) })
-
-    }
-
-    fun centerOnMe(myLocation: GeoPoint?) {
-        if (myLocation != null)
-            map.controller.animateTo(myLocation, 3.0, 2)
     }
 
 
@@ -162,7 +145,7 @@ class MapFragment() : Fragment() {
     override fun onPause() {
         super.onPause()
         locationOverlay.enableMyLocation()
-        map.onPause()  //needed for compass, my location overlays, v6.0.0 and up
+        map.onPause()
     }
 }
 
