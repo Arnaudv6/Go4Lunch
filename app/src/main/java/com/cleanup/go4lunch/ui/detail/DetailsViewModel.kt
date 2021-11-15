5a1b150eeb2ba8471c980f5a09c532bed2a02d07
Arnaudv6 <Arnaudv6@users.noreply.github.com>
2021-11-15 11:15:02 +0100

    fix details screen waits for connection timeout to load if users server can't be reached

--- app/src/main/java/com/cleanup/go4lunch/ui/detail/DetailsViewModel.kt
+++ app/src/main/java/com/cleanup/go4lunch/ui/detail/DetailsViewModel.kt
@@ -2,18 +2,14 @@ package com.cleanup.go4lunch.ui.detail
 
 import android.content.Context
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.SavedStateHandle
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
+import androidx.lifecycle.*
 import com.cleanup.go4lunch.R
 import com.cleanup.go4lunch.data.pois.PoiRepository
 import com.cleanup.go4lunch.data.useCase.UseCase
 import com.cleanup.go4lunch.ui.PoiMapperDelegate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,17 +26,30 @@ class DetailsViewModel
     private val colorInactive = ContextCompat.getColor(appContext, R.color.grey)
     private val colorGold = ContextCompat.getColor(appContext, R.color.gold)
 
+    private val poiEntityFlow = flow {
+        val id = savedStateHandle.get<Long>(DetailsActivity.OSM_ID)
+        if (id != null) { // todo Nino : là, je serais tenté de mettre assert()?
+            val poi = poiRepository.getPoiById(id)
+            if (poi != null) emit(poi)
+        }
+    }
+
+    private val colleaguesListFlow: StateFlow<List<DetailsViewState.Item>> = poiEntityFlow.map {
+        useCase.usersGoingThere(it.id).map { user ->
+            DetailsViewState.Item(
+                mateId = user.id,
+                imageUrl = user.avatarUrl ?: "",
+                text = user.firstName
+            )
+        }
+    }.stateIn(viewModelScope, SharingStarted.Eagerly, emptyList())
+
     val viewStateLiveData: LiveData<DetailsViewState> =
         combine(
-            flow {
-                val id = savedStateHandle.get<Long>(DetailsActivity.OSM_ID)
-                if (id != null) { // todo Nino : là, je serais tenté de mettre assert()?
-                    val poi = poiRepository.getPoiById(id)
-                    if (poi != null) emit(poi)
-                }
-            },
-            useCase.sessionUserFlow
-        ) { poi, session ->
+            poiEntityFlow,
+            useCase.sessionUserFlow,
+            colleaguesListFlow,
+        ) { poi, session, colleagues ->
             DetailsViewState(
                 name = poi.name,
                 goAtNoonColor = if (session?.user?.goingAtNoon == poi.id) colorGold else colorInactive,
@@ -55,19 +64,9 @@ class DetailsViewModel
                 website = poi.site.orEmpty(),
                 websiteColor = if (poi.site.isNullOrEmpty()) colorInactive else colorActive,
                 websiteActive = !poi.site.isNullOrEmpty(),
-                neighbourList = getNeighbourList(poi.id)
+                neighbourList = colleagues
             )
         }.asLiveData()
-
-    private fun getNeighbourList(osmId: Long): List<DetailsViewState.Item> {
-        return useCase.usersGoingThere(osmId).map {
-            DetailsViewState.Item(
-                mateId = it.id,
-                imageUrl = it.avatarUrl ?: "",
-                text = it.firstName
-            )
-        }
-    }
 
     fun goingAtNoonClicked() {
         // todo interpolation
